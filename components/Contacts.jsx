a83e5742b790c2c0860d0ef68b8342a9c837b57d
Stefan Tórz <storz@interia.eu>
2018-02-28 16:14:16 +0100

    change files

---
+++ components/Contacts.jsx
@@ -0,0 +1,22 @@
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+
+    var contacts = this.props.items.map(function(contact) {
+        return (
+          <div className={'Contact'}> 
+            item: contact, 
+            key: contact.id
+          </div>
+    );
+
+    return (
+      <ul className={'contactsList'}>
+        contacts
+      </ul>
+    );
+  }
+});
